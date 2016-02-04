775c439141974cf280f389bae3254c75736264fe
Commander-lol <ljcapitanio@gmail.com>
2016-02-04 13:31:56 +0000

    Pedantry

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+		path = require('path'),
+		favicon = require('serve-favicon'),
+		logger = require('morgan'),
+		cookieParser = require('cookie-parser'),
+		bodyParser = require('body-parser'),
 
-var routes = require('./routes/index');
-var tests = require('./routes/testing');
+		routes = require('./routes/index'),
+		tests = require('./routes/testing'),
 
-var app = express();
+		app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
